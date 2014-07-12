9b157c39d69e631bdd85e78e427913ec275ebf2e
alexshook <alexandra.shook@gmail.com>
2014-07-12 18:34:56 -0400

    moving resforce into module

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,2 @@
 module ApplicationHelper
-
-  def client
-    @client ||= Restforce.new :oauth_token => current_user.oauth_token,
-    :refresh_token => current_user.refresh_token,
-    :instance_url  => current_user.instance_url,
-    :client_id     => ENV['SALESFORCE_KEY'],
-    :client_secret => ENV['SALESFORCE_SECRET']
-  end
-
 end
